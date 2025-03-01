73284f37d1a67b18df0b83c29a328b6dba294e2c
2391454607 <2391454607@qq.com>
2025-03-01 18:45:03 +0800

    Add files via upload

---
+++ server/src/test/java/com/html/gumeng/GumengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.html.gumeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GumengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
