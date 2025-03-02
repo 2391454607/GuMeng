6cd572dfb10530c4b7dcd89ed99f884e1527b62d
zzj <2391454607@qq.com>
2025-03-02 16:05:53 +0800

    处理@符号

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
-// 获取当前文件的路径
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import {URL} from "node:url";
 
 export default defineConfig({
   plugins: [vue()],
+
+  /**
+   * @description 处理'@'符号
+   */
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
+
