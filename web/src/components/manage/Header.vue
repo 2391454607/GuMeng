74dd0cd63ed034ce07c333fd0bf833733a44ca76
zzj <2391454607@qq.com>
2025-03-03 23:33:22 +0800

    前端管理系统框架

--- web/src/components/manage/Header.vue
+++ web/src/components/manage/Header.vue
@@ -2,6 +2,7 @@
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons-vue';
 import { computed } from 'vue';
 import { useCollapsedStore } from '@/stores/index.js'; // 引入 Pinia Store
+import { UserOutlined } from "@ant-design/icons-vue";
 
 const collapsedStore = useCollapsedStore();
 const collapsed = computed(() => collapsedStore.collapsed); // 使用 computed 确保响应性
@@ -14,13 +15,22 @@ console.log('collapsed:', collapsed.value); // 在 Header 组件中
 </script>
 
 <template>
-  <a-layout-header style="background: #fff; padding: 0">
-    <menu-unfold-outlined
-        v-if="collapsed"
-        class="trigger"
-        @click="toggleCollapsed"
-    />
-    <menu-fold-outlined v-else class="trigger" @click="toggleCollapsed" />
+  <a-layout-header style="background: #fff; padding: 0; display: flex; justify-content: space-between; align-items: center;">
+    <div>
+      <menu-unfold-outlined
+          v-if="collapsed"
+          class="trigger"
+          @click="toggleCollapsed"
+      />
+      <menu-fold-outlined v-else class="trigger" @click="toggleCollapsed" />
+    </div>
+    <a-space class="avatar" direction="vertical" :size="32">
+      <a-space wrap :size="16">
+        <a-avatar size="large">
+          <template #icon><UserOutlined /></template>
+        </a-avatar>
+      </a-space>
+    </a-space>
   </a-layout-header>
 </template>
 
@@ -35,5 +45,9 @@ console.log('collapsed:', collapsed.value); // 在 Header 组件中
 
 .trigger:hover {
   color: #1890ff;
+}
+
+.avatar {
+  margin-right: 64px; /* 调整头像与右侧的距离 */
 }
 </style>
--- web/src/components/manage/Sider.vue
+++ web/src/components/manage/Sider.vue
@@ -14,15 +14,15 @@ console.log('collapsed:', collapsed.value); // 在 Sider 组件中
   <a-layout-sider class="sider" :collapsed="collapsed" :trigger="null" collapsible>
     <div class="logo"/>
     <a-menu class="menu" v-model:selectedKeys="selectedKeys" mode="inline" theme="dark">
-      <a-menu-item class="item" key="1">
+      <a-menu-item key="1" class="item">
         <user-outlined/>
         <span>nav 1</span>
       </a-menu-item>
-      <a-menu-item class="item" key="2">
+      <a-menu-item key="2" class="item">
         <user-outlined/>
         <span>nav 2</span>
       </a-menu-item>
-      <a-menu-item class="item" key="3">
+      <a-menu-item key="3" class="item">
         <user-outlined/>
         <span>nav 3</span>
       </a-menu-item>
@@ -33,12 +33,30 @@ console.log('collapsed:', collapsed.value); // 在 Sider 组件中
 <style scoped>
 .sider {
   min-height: calc(100vh);
+  background-color: #2e2249; /* 深紫色背景 */
 }
+
 .logo {
   height: 32px;
-  background: rgba(255, 255, 255, 0.3);
+  background: rgba(255, 255, 255, 0.3); /* 透明背景 */
   margin: 16px;
 }
 
+.menu {
+  background-color: #2e2249; /* 深紫色背景 */
+}
 
+/* 使用深度选择器覆盖 Ant Design 的样式 */
+:deep(.menu .ant-menu-item) {
+  color: #a8a8a8; /* 默认文字颜色 */
+}
+
+:deep(.menu .ant-menu-item-selected) {
+  background-color: #7c5cff; /* 选中背景色 */
+  color: #ffffff; /* 选中文字颜色 */
+}
+
+:deep(.menu .ant-menu-item::after) {
+  border-right: 3px solid #7c5cff; /* 选中项右侧高亮颜色 */
+}
 </style>
