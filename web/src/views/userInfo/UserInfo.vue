94b6d514a11a6a4c3c4a3dac1f315087a4ab74ed
zzj <2391454607@qq.com>
2025-04-28 17:36:19 +0800

    图标修改

--- web/src/views/userInfo/UserInfo.vue
+++ web/src/views/userInfo/UserInfo.vue
@@ -11,9 +11,9 @@ import {
   IconUnorderedList,
   IconStar,
   IconHistory,
-  IconSettings
+  IconSettings,
+  IconRelation
 } from '@arco-design/web-vue/es/icon';
-import background from "three/src/renderers/common/Background.js";
 
 // 存储用户信息
 const userInfo = ref({});
@@ -92,7 +92,7 @@ const menuItems = [
   { name: '我的订单', icon: IconUnorderedList, url: '/userInfo/order' },
   { name: '我的收藏', icon: IconStar, url: '/userInfo/collection' },
   { name: '浏览历史', icon: IconHistory, url: '/userInfo/history' },
-  { name: '资产流动', icon: IconUser, url: '/userInfo/assetFlows' },
+  { name: '资产流动', icon: IconRelation, url: '/userInfo/assetFlows' },
   { name: '账号设置', icon: IconSettings, url: '/userInfo/settings' }
 ];
 
