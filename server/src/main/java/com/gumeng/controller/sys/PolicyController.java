9c5b962260314ad101cacb25776f65e1d56f45ce
zzj <2391454607@qq.com>
2025-05-08 14:10:18 +0800

    字段修改、政策时间修改

--- server/src/main/java/com/gumeng/controller/sys/PolicyController.java
+++ server/src/main/java/com/gumeng/controller/sys/PolicyController.java
@@ -12,11 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Base64;
-import java.util.Date;
 
 /**
  * 功能：政策信息后台管理
