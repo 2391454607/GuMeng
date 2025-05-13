28d607d511234393605f3a572380461b1db49455
zzj <2391454607@qq.com>
2025-05-13 19:46:49 +0800

    文件移动

--- server/src/main/java/com/gumeng/controller/web/IchController.java
+++ server/src/main/java/com/gumeng/controller/web/IchController.java
@@ -1,4 +1,4 @@
-package com.gumeng.controller.ich;
+package com.gumeng.controller.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
