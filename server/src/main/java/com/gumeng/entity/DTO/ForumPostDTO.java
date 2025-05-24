7c9d4f6ace26259954ce2dedaa8ad6b4ce4d0651
Lorn <2630933564@qq.com>
2025-05-24 16:33:37 +0800

    帖子字数增加

--- server/src/main/java/com/gumeng/entity/DTO/ForumPostDTO.java
+++ server/src/main/java/com/gumeng/entity/DTO/ForumPostDTO.java
@@ -23,7 +23,7 @@ public class ForumPostDTO {
      * 帖子内容
      */
     @NotBlank(message = "内容不能为空")
-    @Size(min = 10, max = 2000, message = "内容长度需在10-2000个字符之间")
+    @Size(min = 10, max = 5000, message = "内容长度需在10-5000个字符之间")
     private String content;
 
     /**
