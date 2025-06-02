fd2bec897e7adc5ff5b3815650034b7c942fa5ad
Lorn <2630933564@qq.com>
2025-06-02 18:19:06 +0800

    非遗详情

--- web/src/api/web/IchProject.js
+++ web/src/api/web/IchProject.js
@@ -6,3 +6,8 @@ export const getProjectList = async ({current: current,size: size}) => {
     return http.get("/web/ich/getProject", {current: current,size: size})
 }
 
+//获取非遗项目详情
+export const getProjectDetail = async (id) => {
+    return http.get(`/web/ich/getProjectDetail/${id}`);
+}
+
