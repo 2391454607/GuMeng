20b600047c834dbf22af18cd3084c73eab033de9
zzj <2391454607@qq.com>
2025-06-09 19:30:37 +0800

    用户管理bug修改

--- server/src/main/java/com/gumeng/service/sys/impl/SysUserServiceImpl.java
+++ server/src/main/java/com/gumeng/service/sys/impl/SysUserServiceImpl.java
@@ -1,9 +1,17 @@
 package com.gumeng.service.sys.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gumeng.domain.user.User;
+import com.gumeng.mapper.user.UserMapper;
+import com.gumeng.service.sys.SysUserService;
+import org.springframework.stereotype.Service;
+
 /**
  * 功能：
  * 作者：Z
  * 日期：2025/4/21 下午7:56
  */
-public class SysUserServiceImpl {
+@Service
+public class SysUserServiceImpl extends ServiceImpl<UserMapper, User> implements SysUserService {
+
 }
